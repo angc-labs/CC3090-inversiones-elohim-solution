cda0302edfa8692525e5137e8892d6fc130e7635
angcoder-c <angcoder.c@gmail.com>
2026-03-23 19:26:53 -0600

    init .net api

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/ping", () => Results.Ok(new { message = "pong" }));
+
+app.Run();
